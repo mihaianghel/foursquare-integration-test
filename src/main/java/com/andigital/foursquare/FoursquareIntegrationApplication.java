295f12de4036601754772a191c90ee769b836e42
“Mihai <anghelmihai88@yahoo.com>
2015-11-14 09:16:24 +0000

    First commit

---
+++ src/main/java/com/andigital/foursquare/FoursquareIntegrationApplication.java
@@ -0,0 +1,12 @@
+package com.andigital.foursquare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoursquareIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoursquareIntegrationApplication.class, args);
+    }
+}
